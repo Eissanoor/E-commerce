f8fbcc6af0807bbdfdb6abb914ee5f9c770dfb6f
eissa <eissaanoor@gmail.com>
2021-11-27 01:04:15 -0800

    My first commit

--- db/cons.js
+++ db/cons.js
@@ -3,8 +3,8 @@ const MONGODB_URL = 'mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net
 mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useFindAndModify: false
 
 
 }).then(() => {
