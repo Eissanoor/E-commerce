f5e8e64528c50c4ce5a833c05bdea887afd40a94
eissa <eissaanoor@gmail.com>
2021-11-27 00:52:56 -0800

    My first commit

--- db/cons.js
+++ db/cons.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const MONGODB_URL = 'mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/khaaaad?retryWrites=true&w=majority';
 mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
 
 }).then(() => {
     console.log("good ho gaya");
