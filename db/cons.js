ec5869daa34a0eaccf4470d1ff535100c101180d
eissa <eissaanoor@gmail.com>
2021-11-27 10:34:34 -0800

    My first commit

--- db/cons.js
+++ db/cons.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
-    // const MONGODB_URL = 'mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/khaaaad?retryWrites=true&w=majority';
+mongoose.Promise = global.Promise;
+// const MONGODB_URL = 'mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/khaaaad?retryWrites=true&w=majority';
 mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/khaaaad?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
     // useFindAndModify: false
-    useMongoClient: true
+    // useMongoClient: true
 
 }).then(() => {
     console.log("good ho gaya");
