48f9e9ba99ffb55b1c748450d11950d301957934
eissa <eissaanoor@gmail.com>
2021-11-30 19:51:36 -0800

    My first commit

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
 
     serviceID: "VAd4349cab3256c69ac3c4f13b421f6736",
     accountID: "ACb0484f86587d176ba1903515b84632bc",
-    authToken: "bd425b6ea9f1d3a95940c505c0af72c2",
+    authToken: "ab998b41244f5b260af989c1e5e7e9e8",
 
 }
