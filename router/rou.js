8f498bcd90e54697e25bef11334844990c617e1e
Eisaanoor <eissaanoor@gmail.com>
2021-12-22 16:52:27 -0800

    kjsadh

---
+++ router/rou.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const router = new express.Router();
+
+
+
+
+// router.get("/eisa", (req,res)=>{
+
+
+//     res.send("this is eissanoor");
+
+// });
+
+// module.exports = router;
---
+++ router/rou2.js
@@ -0,0 +1,24 @@
+// const express = require("express");
+// const router = new express.Router();
+
+
+
+
+// router.get("/eisa2", (req,res)=>{
+
+
+//     res.send("this is eissanoor");
+
+// });
+
+
+// router.get("/e", (req,res)=>{
+
+
+//     res.send("the send router");
+
+// });
+
+
+
+// module.exports = router;
